e7c5a418e926cfa3d5fd1d9cf9aafcb4e22d7410
Prince <princecallora221b@gmail.com>
2024-12-12 15:12:45 +0800

    added solutions to this activity

---
+++ foundations/pairProgrammingTOP/index.js
@@ -0,0 +1,19 @@
+const spoonInventory = [
+    "Wooden Spoon",
+    "Silver Spoon",
+    "Wooden Spoon",
+    "Soup Spoon",
+    "Silver Spoon"
+]
+
+let count = 0;
+
+//loop through the array and print the items
+function currentSpoon() {
+    const spoonCount = 0;
+    for (let i = 0; i < array.length; i++) {
+        const currentSpoon = spoonInventory[i];  
+    }
+}
+
+currentSpoon();
